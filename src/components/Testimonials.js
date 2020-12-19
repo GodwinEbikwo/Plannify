ca0f67aa1ee26304917442310628c91a02d20997
“GodwinEbikwo” <godwin.a.ebikwo@gmail.com>
2020-12-19 05:21:58 +0000

    testimonial added

---
+++ src/components/Testimonials.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Testimonials() {
+  return (
+    <section data-scroll-section className="testimonials">
+      <div className="container container--pall">
+        <div className="inner-containers">
+          <h1>
+            Here what our <span className="accent-line"> customers</span> have
+            been saying about us
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+}
